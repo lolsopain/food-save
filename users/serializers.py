f25b7c677bef317d4ef21d0e3ece503e07684964
lolsopain <lolsopain@gmail.com>
2026-05-23 19:23:45 +0500

    fix: cleaned up user serializer and resolved merge conflict

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
-from .models import User
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # 'phone' bu yerdan olib tashlandi
         fields = ['id', 'username', 'email', 'role', 'password']
         extra_kwargs = {
-            'password': {'write_only': True, 'required': True}, # Parolni majburiy qildik
-            'email': {'validators': []},
+            'password': {'write_only': True, 'required': True},
+            'email': {'validators': []}
         }
 
     def validate_email(self, value):
@@ -32,7 +32,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        # create_user ichidan ham phone olib tashlandi
         user = User.objects.create_user(
             username=validated_data.get('username'),
             email=validated_data.get('email'),
@@ -40,40 +39,3 @@ class UserSerializer(serializers.ModelSerializer):
             password=password
         )
         return user
-    def validate_email(self, value):
-        if not value:
-            raise serializers.ValidationError("Email maydoni bo'sh bo'lishi mumkin emas!")
-        
-        user_id = self.instance.id if self.instance else None
-        email_count = User.objects.filter(email=value).exclude(id=user_id).count()
-        
-        if email_count >= 3:
-            raise serializers.ValidationError("Bu email manziliga maksimal 3 ta akkaunt biriktirilishi mumkin!")
-        
-        return value
-
-    def validate_username(self, value):
-        user_id = self.instance.id if self.instance else None
-        if User.objects.filter(username=value).exclude(id=user_id).exists():
-            raise serializers.ValidationError("Ushbu username band! Boshqa nom kiriting.")
-        return value
-
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        # Frontenddan phone kelmasa, bo'sh matn sifatida saqlaydi
-        user = User.objects.create_user(
-            username=validated_data.get('username'),
-            email=validated_data.get('email'),
-            phone=validated_data.get('phone', ''),
-            role=validated_data.get('role', 'client'),
-            password=password
-        )
-        return user
-
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        data['user_id'] = self.user.id
-        data['username'] = self.user.username
-        data['token'] = data['access'] 
-        return data
